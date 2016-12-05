9d1ef293ca1ef477970f513c151252074937192e
Yubo Wang <yubow2@uw.edu>
2016-12-05 14:52:15 -0800

    listed out problems with certain states

--- soichi-test.r
+++ soichi-test.r
@@ -21,6 +21,18 @@ colnames(final_data) <- c('state', 'abb', 'county', 'party', 'candidate', 'votes
 # View(final_data)
 nrow(final_data) #17479 / 24611 (over 7000 missing)
 
+## We won't have Alaska's data because Alaska does not participate in the primary elections.
+## Missing states (not including alaska):
+#     North Dakota
+#     Massachussetts
+#     Connecticut
+#     Kansas
+#     Maine
+#     Minnesota
+#     New Hampshire
+#     Rhode Island
+#     Vermont
+#     Alaska
 
 #combining data by state
 bernie_by_state <- ByState(final_data, "Bernie Sanders")
