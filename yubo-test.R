4670cc24bf5d4021bb742062c01a9f044c2b3e6f
Yubo Wang <yubow2@uw.edu>
2016-12-06 15:14:18 -0800

    fixed data issues

--- yubo-test.R
+++ yubo-test.R
@@ -3,7 +3,7 @@ library(dplyr)
 data <- read.csv('./data/primary_results.csv', stringsAsFactors = FALSE)
 county.facts <- read.csv('./data/county_facts.csv', stringsAsFactors = FALSE)
 
-SortData <- function(results, facts) {
+SortData <- function(facts) {
   new.county <- facts %>% 
                 filter(state_abbreviation != "")
   
@@ -20,14 +20,42 @@ SortData <- function(results, facts) {
   return(new.county)
 }
 
-new.data <- SortData(data, county.facts)
+new.data <- SortData(county.facts)
 data$county <- tolower(data$county)
-join_new_data <- left_join(data, new.data, by=c("county", "state_abbreviation"))
+join_new_data <- left_join(data, new.data, by = c("county", "state_abbreviation"))
 
 final_data_yubo <- join_new_data %>%  na.omit() %>%
   select(state, state_abbreviation, county, party, candidate, votes,
          SEX255214, RHI225214, RHI325214, RHI425214, RHI525214, RHI625214,
          RHI725214, RHI825214, EDU635213, EDU685213, INC110213)
 colnames(final_data_yubo) <- c('state', 'abb', 'county', 'party', 'candidate', 'votes',
-                          'female', 'black', 'indian', 'asian', 'hawaiian', 'multi', 'hispanic',
-                          'white', 'highschool', 'bachelors', 'income')
+                               'female', 'black', 'indian', 'asian', 'hawaiian', 'multi', 'hispanic',
+                               'white', 'highschool', 'bachelors', 'income')
+
+#############################################################
+# Manually adding in Louisiana and New Hampshire
+
+temp.primary <- data %>%
+                filter(state_abbreviation == 'LA' | state_abbreviation == 'NH')
+
+temp.county <- county.facts %>% 
+                filter(state_abbreviation == 'LA' | state_abbreviation == 'NH')
+
+new.temp.county <- SortData(temp.county)
+temp.primary$county <- tolower(temp.primary$county)
+temp_join_LA <- left_join(temp.primary, new.temp.county, by=c("fips")) %>% filter(state_abbreviation.x == 'LA')
+temp_join_LA <- temp_join_LA %>% select(-county.y, -state_abbreviation.y, -fips)
+names(temp_join_LA)[names(temp_join_LA) == "state_abbreviation.x"] <- "state_abbreviation"
+names(temp_join_LA)[names(temp_join_LA) == "county.x"] <- "county"
+
+temp_join_NH <- left_join(temp.primary, new.temp.county, by=c("county", "state_abbreviation")) %>% filter(state_abbreviation == 'NH')
+temp_join_NH <- temp_join_NH %>% select(-fips.x, -fips.y)
+
+joined_data <- rbind(temp_join_LA, temp_join_NH) %>% 
+                select(state, state_abbreviation, county, party, candidate, votes,
+                       SEX255214, RHI225214, RHI325214, RHI425214, RHI525214, RHI625214,
+                       RHI725214, RHI825214, EDU635213, EDU685213, INC110213)
+colnames(joined_data) <- c('state', 'abb', 'county', 'party', 'candidate', 'votes',
+                               'female', 'black', 'indian', 'asian', 'hawaiian', 'multi', 'hispanic',
+                               'white', 'highschool', 'bachelors', 'income')
+#############################################################
